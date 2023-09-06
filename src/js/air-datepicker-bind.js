d53137fc3c33ba803c46fdadbc4d175ffead0244
TrojanDll <dzafarovt36@gmail.com>
2023-09-06 12:54:31 +0300

    feat: calendar adaptation added

--- src/js/air-datepicker-bind.js
+++ src/js/air-datepicker-bind.js
@@ -1,4 +1,16 @@
-new AirDatepicker("#date-form-report1");
-new AirDatepicker("#date-form-report2");
-new AirDatepicker("#date-form-report3");
-new AirDatepicker("#date-form-report4");
+new AirDatepicker("#date-form-report1", {
+    isMobile: !!(window.innerWidth <= 680),
+    autoClose: !!(window.innerWidth <= 680),
+});
+new AirDatepicker("#date-form-report2", {
+    isMobile: !!(window.innerWidth <= 680),
+    autoClose: !!(window.innerWidth <= 680),
+});
+new AirDatepicker("#date-form-report3", {
+    isMobile: !!(window.innerWidth <= 680),
+    autoClose: !!(window.innerWidth <= 680),
+});
+new AirDatepicker("#date-form-report4", {
+    isMobile: !!(window.innerWidth <= 680),
+    autoClose: !!(window.innerWidth <= 680),
+});
